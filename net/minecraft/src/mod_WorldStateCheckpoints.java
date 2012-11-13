1f3944b3744059c6ad4f965b3fa773f4b7625260
bspkrs <bspkrs@gmail.com>
2012-11-13 16:53:26 -0500

    1.4.4 update
    added mcmod.info

--- net/minecraft/src/mod_WorldStateCheckpoints.java
+++ net/minecraft/src/mod_WorldStateCheckpoints.java
@@ -13,75 +13,75 @@ public class mod_WorldStateCheckpoints extends BaseMod
 {
     @MLProp(info = "Set to true to allow checking for mod updates, false to disable")
     public static boolean     allowUpdateCheck     = true;
-    
+
     public static String      menuKeyStr           = "F6";
     public static String      saveKeyStr           = "F7";
-    
+
     public static KeyBinding  menuKey              = new KeyBinding("CheckpointsMenu", Keyboard.getKeyIndex(menuKeyStr));
     public static KeyBinding  saveKey              = new KeyBinding("CheckpointsSave", Keyboard.getKeyIndex(saveKeyStr));
     public static boolean     justLoadedCheckpoint = false;
     public static boolean     justLoadedWorld      = false;
     public static String      loadMessage          = "";
     public CheckpointManager  cpm;
-    
+
     private boolean           checkUpdate;
     private ModVersionChecker versionChecker;
-    private String            versionURL           = "https://dl.dropbox.com/u/20748481/Minecraft/1.4.2/worldStateCheckpoints.version";
+    private String            versionURL           = "https://dl.dropbox.com/u/20748481/Minecraft/1.4.4/worldStateCheckpoints.version";
     private String            mcfTopic             = "http://www.minecraftforum.net/topic/1548243-";
-    
+
     private Minecraft         mc;
-    
+
     @Override
     public String getName()
     {
         return "WorldStateCheckpoints";
     }
-    
+
     @Override
     public String getVersion()
     {
-        return "ML 1.4.2.r03";
+        return "ML 1.4.4.r01";
     }
-    
+
     public mod_WorldStateCheckpoints()
     {
         checkUpdate = allowUpdateCheck;
         versionChecker = new ModVersionChecker(getName(), getVersion(), versionURL, mcfTopic, ModLoader.getLogger());
-        
+
         mc = ModLoader.getMinecraftInstance();
-        
+
         /*
          * for (int i = 0; i < Keyboard.getKeyCount(); i++)
          * System.out.println("Key Index " + i + " : " +
          * Keyboard.getKeyName(i));
          */
-        
+
     }
-    
+
     @Override
     public void load()
     {
         versionChecker.checkVersionWithLogging();
-        
+
         ModLoader.registerKey(this, menuKey, false);
         ModLoader.registerKey(this, saveKey, false);
         ModLoader.addLocalization(menuKey.keyDescription, "Checkpoints Menu");
         ModLoader.addLocalization(saveKey.keyDescription, "Checkpoints Quick Save");
         ModLoader.setInGameHook(this, true, true);
     }
-    
+
     @Override
     public void clientConnect(NetClientHandler var1)
     {
         justLoadedWorld = true;
     }
-    
+
     @Override
     public void clientDisconnect(NetClientHandler var1)
-    {   
-        
+    {
+
     }
-    
+
     @Override
     public boolean onTickInGame(float f, Minecraft mc)
     {
@@ -92,35 +92,35 @@ public class mod_WorldStateCheckpoints extends BaseMod
                     mc.thePlayer.addChatMessage(msg);
             checkUpdate = false;
         }
-        
+
         if (justLoadedWorld)
         {
             cpm = null;
             justLoadedWorld = false;
         }
-        
+
         if (cpm == null)
         {
             cpm = new CheckpointManager(mc);
         }
-        
+
         if (justLoadedCheckpoint)
         {
             mc.thePlayer.addChatMessage(loadMessage);
             loadMessage = "";
-            
+
             if (cpm.autoSaveEnabled)
                 cpm.tickCount = 0;
-            
+
             justLoadedCheckpoint = false;
         }
-        
+
         if (cpm.autoSaveEnabled)
             cpm.incrementTickCount();
-        
+
         return true;
     }
-    
+
     @Override
     public void keyboardEvent(KeyBinding event)
     {
